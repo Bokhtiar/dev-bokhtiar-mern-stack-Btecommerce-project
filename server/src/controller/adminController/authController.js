08cb222b07b7478ebad1edc8bbecce67f07939e6
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-07-04 01:32:59 +0600

    backend login and jwt token response done

--- server/src/controller/adminController/authController.js
+++ server/src/controller/adminController/authController.js
@@ -1,5 +1,5 @@
 const admins = require("../../models/admin.model");
-
+const jwt = require('jsonwebtoken')
 /*admin list*/
 const list = async (req, res, next) => {
   try {
@@ -16,25 +16,62 @@ const list = async (req, res, next) => {
 
 /*login*/
 const login = async (req, res, next) => {
-  res.status(201).json({
-    status: true,
-    message: "done",
-  });
+  try {
+    const { email, password } = req.body;
+
+    /*email check */
+    const account = await admins.findOne({ email });
+    if (!account) {
+      res.status(404).json({
+        status: true,
+        message: "Email or Password Invalid...!",
+      });
+    }
+
+    /* Password Check */
+    const existPassword = await admins.findOne({ password });
+    if (!existPassword) {
+      res.status(404).json({
+        status: true,
+        message: "Email or Password Invalid...!",
+      });
+    }
+
+    /* Generate JWT token */
+    const token = await jwt.sign(
+      {
+        id: account._id,
+        name: account.name,
+        role: account.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      status: true,
+      data: token,
+      message: "Login Successfully Done...!",
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 /*Register*/
 const register = async (req, res, next) => {
   try {
-    const { name, email, password, role } = req.body
+    const { name, email, password, role } = req.body;
 
-    const existEmail = await admins.findOne({ email })
+    const existEmail = await admins.findOne({ email });
     if (existEmail) {
       res.status(403).json({
         status: true,
-        message: "Email Already Exist...!"
+        message: "Email Already Exist...!",
       });
     }
-    
+
     const newAdmin = new admins({
       name,
       email,
