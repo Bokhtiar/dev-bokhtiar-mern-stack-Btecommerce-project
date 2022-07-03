6d142255b6cc1ca57c07b34db5c77dc31a411b64
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-07-04 00:44:51 +0600

    backend admin Register Done

--- server/src/controller/adminController/authController.js
+++ server/src/controller/adminController/authController.js
@@ -1,13 +1,59 @@
-const admins = require('../../models/admin.model')
+const admins = require("../../models/admin.model");
+
+/*admin list*/
+const list = async (req, res, next) => {
+  try {
+    const results = await admins.find();
+    res.status(201).json({
+      status: true,
+      data: results,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
 /*login*/
-const login = async(req, res, next) => {
-    res.status(200).json({
-        status: true,
-        message: 'done'
-    })
-}
+const login = async (req, res, next) => {
+  res.status(201).json({
+    status: true,
+    message: "done",
+  });
+};
 
-module.exports ={
-    login
-}
+/*Register*/
+const register = async (req, res, next) => {
+  try {
+    const { name, email, password, role } = req.body
+
+    const existEmail = await admins.findOne({ email })
+    if (existEmail) {
+      res.status(403).json({
+        status: true,
+        message: "Email Already Exist...!"
+      });
+    }
+    
+    const newAdmin = new admins({
+      name,
+      email,
+      password,
+      role,
+    });
+    await newAdmin.save();
+
+    res.status(201).json({
+      status: true,
+      message: "Admin Register Successfully...!",
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+module.exports = {
+  login,
+  register,
+};
--- server/src/router/adminRouter/auth/admin.route.js
+++ server/src/router/adminRouter/auth/admin.route.js
@@ -2,5 +2,6 @@ const adminRoute = require('express').Router()
 const authController = require('../../../controller/adminController/authController')
 
 adminRoute.post('/login', authController.login)
+adminRoute.post('/register', authController.register)
 
 module.exports = adminRoute
