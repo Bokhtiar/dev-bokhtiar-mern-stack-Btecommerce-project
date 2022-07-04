2f61d21e9236c1ba5ebb22e57582952b189e869a
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-07-04 23:28:47 +0600

    user login and token genarate done

--- server/src/controller/userController/userController.js
+++ server/src/controller/userController/userController.js
@@ -1,23 +1,69 @@
 const users = require("../../models/users.model");
+const jwt = require('jsonwebtoken')
 
-/*register */
+/* Login */
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const account = await users.findOne({email})
+    if(!account){
+        res.status(404).json({
+            status: true,
+            message: "Invalid Email || Password"
+        })
+    }
+    /* Password Check */
+    const existPassword = await users.findOne({ password });
+    if (!existPassword) {
+      res.status(404).json({
+        status: true,
+        message: "Email or Password Invalid...!",
+      });
+    }
+
+    /* Generate JWT token */
+    const token = await jwt.sign(
+      {
+        id: account._id,
+        name: account.name,
+        role: account.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      status: true,
+      data: token,
+      message: "User Login Successfully Done...!",
+    });
+
+
+
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+/* register */
 const register = async (req, res, next) => {
   try {
     const { name, email, password, role } = req.body;
 
-    const existUser = await users.findOne({email})
-    if(existUser){
-        res.status(404).json({
-            status: false,
-            message: "Email Already Exists...!"
-        })
+    const existUser = await users.findOne({ email });
+    if (existUser) {
+      res.status(404).json({
+        status: false,
+        message: "Email Already Exists...!",
+      });
     }
     const newUser = new users({
       name,
       email,
       password,
       role,
-    })
+    });
     await newUser.save();
     res.status(201).json({
       status: true,
@@ -30,5 +76,6 @@ const register = async (req, res, next) => {
 };
 
 module.exports = {
+  login,
   register,
 };
--- server/src/router/userRouter/user.route.js
+++ server/src/router/userRouter/user.route.js
@@ -1,6 +1,7 @@
 const userRoute = require('express').Router()
 const userController = require('../../controller/userController/userController')
 
+userRoute.post('/login', userController.login)
 userRoute.post('/register', userController.register)
 
 module.exports = userRoute
